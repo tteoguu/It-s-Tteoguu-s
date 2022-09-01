168a38b59ef09c38f849511665846760e28b03b2
Emlan Implica <111472856+tteoguu@users.noreply.github.com>
2022-09-01 10:23:36 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) { 
+
+
+    Gear helmet = new Gear(); 
+
+        helmet.name = "Tteogu";
+        helmet.defense = 60; 
+        helmet.weight = 2.3f;
+        helmet.description = "One of the noblest thing. A powerful gear to have."; 
+        helmet.rarity = "Legendary";
+        helmet.hasElementalResistance = true;
+        helmet.showDescription();
+        helmet.saMydefense();
+
+    }
+}
---
+++ Gear.java
@@ -0,0 +1,23 @@
+public class Gear { 
+
+    //helmet 
+
+    String name;
+    int defense;
+    float weight;
+    String description; 
+    String rarity;
+    boolean hasElementalResistance;
+
+    public void showDescription() {
+        System.out.println("Description: " + description);
+    }
+
+    public void saMydefense() {
+        System.out.println("My helmet's defense is " + defense);
+    }
+
+    }
+
+
+   
